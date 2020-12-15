79a60813de98e00be9105399fea6f68036f4c5c8
Nikola Nikushev <nikola.nikushev@hotmail.com>
2020-12-15 21:38:44 +0200

    commit: Added check for github token

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Logger } from 'sitka';
+import dotEnv from 'dotenv';
+dotEnv.config();
+
+if (!process.env.GITHUB_TOKEN || !process.env.PRIVATE_REPO_TOKEN) {
+  throw new Error(
+    'Please specify the GITHUB_TOKEN environemtn variable in .env'
+  );
+}
 
 export class Example {
   /* Private Instance Fields */
 
-  private _logger: Logger;
+  private readonly _logger: Logger;
 
   /* Constructor */
 
