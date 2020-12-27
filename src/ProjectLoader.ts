7cdeec945ff058fe0b9b6b03689bb3b5c8b57670
Nikola Nikushev <nikola.nikushev@hotmail.com>
2020-12-27 22:13:27 +0200

    internal: Updated CSV values

--- src/ProjectLoader.ts
+++ src/ProjectLoader.ts
@@ -60,6 +60,7 @@ export class ProjectLoader {
             continue;
           }
           const weekCommitsForAuthor = {
+            owner: this.owner,
             repo: this.repoName,
             author: stat.author.login,
             week: week.w,
